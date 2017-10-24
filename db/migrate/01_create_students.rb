bed5e09b297105c42a658dce9e91f4ae1f442f23
Lisa Lee <snickers495@gmail.com>
2017-10-24 18:56:06 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+    t.string :name
+    t.string :genre
+    t.integer :age
+    t.string :hometown
+  end
+  end
 end
